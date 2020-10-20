abc14bd5c0af88d5da34ba55f554f55746325c53
Hayden Barnes <p.hayden.barnes@outlook.com>
2020-10-20 09:54:01 -0600

    Added admin login

--- TheDadsDepot/Startup.cs
+++ TheDadsDepot/Startup.cs
@@ -54,9 +54,13 @@ namespace TheDadsDepot
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDeveloperExceptionPage();
-            app.UseStatusCodePages();
-            app.UseStaticFiles();
+            if(env.IsProduction()) {
+                app.UseExceptionHandler("/error");
+            } else
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+            }
             app.UseSession();
             app.UseRouting();
 
